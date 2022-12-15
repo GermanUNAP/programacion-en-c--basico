cff854fb4de39cec2eb38037f252fd3272f528ac
germanUNAP <ccus.german.chura@gmail.com>
2022-12-16 03:27:36 +1300

    cin, cout and commnets

---
+++ in_and_out.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    // comment for one line
+    int age = 0;
+    cout<<"the initial age is: "<<edad<<endl;
+    cout<<"Enter tour age: "<<endl;
+    cin>>edad;
+    cout<<"Your age is: "<<edad<<endl;
+}
