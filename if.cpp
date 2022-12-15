0b78e8f059fa049a17cb55e61f38546abd15402f
germanUNAP <ccus.german.chura@gmail.com>
2022-12-16 03:32:20 +1300

    conditional if

---
+++ if.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+using namespace std;
+
+int main(){
+    int ageKid = 10; 
+    int ageMother = 35;
+    if(ageKid >= 18){
+        cout<<"the kid can vote"<<endl;
+    }
+    else{
+        cout<<"the kid can not vote"<<endl;
+    }
+    if(ageMother >= 18){
+        cout<<"The mother can vote"<<endl;
+    }
+    else{
+        cout<<"The mother can not vote"<<endl;
+    }
+    return 0;
+}
