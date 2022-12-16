9b3a3c079edfafd214bad7d7549f778fdcc69d3b
germanUNAP <ccus.german.chura@gmail.com>
2022-12-17 02:50:14 +1300

    funcions part added

---
+++ functions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int suma(int a, int b){
+    return a + b;
+}
+int main(){
+    int a = 1, b=2;
+    cout<<suma(a, b);
+    //saludar('holas');
+    return 0;
+}
